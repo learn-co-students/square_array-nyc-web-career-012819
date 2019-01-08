fa78e5bfa28fa37f172c4d13f82f7366e39b0ee8
Kyung Won Park <stomg7969@gmail.com>
2019-01-08 02:24:02 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,12 @@
 def square_array(array)
   arr = []
-  array.each {|i| arr << i**2}
+  array.each {|i| arr.push(i**2)}
   arr
+  
+  #option 1 -----
+  #arr = array.collect{|i| i**2}
+  #arr
+  
+  #option 2 -----
+  #array.collect!{|i| i**2}
 end
